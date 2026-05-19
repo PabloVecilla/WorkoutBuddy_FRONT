310d7ce43938171ba6e97e552ba767818626adf2
PabloVecilla <pablovecilla8@gmail.com>
2026-05-19 08:23:22 +0200

    css

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-// import styles from "./RegisterPage.module.css"; 
+import styles from "./RegisterPage.module.css"; 
 
 import apiClient from "../../api/client";
 
